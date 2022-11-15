df73adae7ebae1f7506e97a0e50ef8b9169d78c8
sarao <saraok@metropolia.fi>
2022-11-15 10:44:49 +0200

    passport

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
 
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 //not for próject
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 const username = "foo";
 const password = "bar";
@@ -19,15 +29,18 @@ app.use(session({ secret: "Hauki on kala." }));
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.post("/login", (req, res) => {
-  const passwd = req.body.password;
-  const usern = req.body.username;
-  if (usern === username && passwd === password) {
-    res.session.kirjautunut = true;
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/form" }),
+  (req, res) => {
+    console.log("success");
     res.redirect("/secret");
-  } else {
-    res.redirect("/form");
   }
+);
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 app.get("/", (req, res) => {
@@ -38,12 +51,8 @@ app.get("/form", (req, res) => {
   res.render("form");
 });
 
-app.get("/secret", (req, res) => {
-  if (req.session.kirjautunut) {
-    res.render("secret");
-  } else {
-    res.redirect("/form");
-  }
+app.get("/secret", loggedIn, (req, res) => {
+  res.render("secret");
 });
 
 app.get("/setCookie/:clr", (req, res) => {
